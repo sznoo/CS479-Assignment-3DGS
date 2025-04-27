5d5438fab83486332aa044ee4c396017f214d9e7
SeungWoo Yoo <dreamy1534@gmail.com>
2025-04-27 10:32:19 +0900

    Update GPU ID in runner script

--- render_all.sh
+++ render_all.sh
@@ -4,5 +4,5 @@ scene_names=("chair" "lego" "materials" "drums")
 for scene_name in "${scene_names[@]}"
 do
     echo "Rendering scene: $scene_name";
-    CUDA_VISIBLE_DEVICES=8 python render.py --scene-type $scene_name;
+    CUDA_VISIBLE_DEVICES=0 python render.py --scene-type $scene_name;
 done
